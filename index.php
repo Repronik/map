784b223f60b8900ee63de0848333ae98049d7492
HSoeren <soeren.helms1@gmail.com>
2018-06-10 19:12:59 +0200

    Updated index
    Changed Names and upgraded to PHP to support multiple languages and some other things later

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta content="utf-8" http-equiv="encoding">
-        <title>cuteboy.world</title>
+        <title>Repronik Map Test</title>
         <script src="resources/javascript/openlayers.js" type="text/javascript"></script>
         <script type="text/javascript" src="resources/javascript/main.js"></script>
         <link rel="stylesheet" type="text/css" href="resources/css/openlayers.css">
